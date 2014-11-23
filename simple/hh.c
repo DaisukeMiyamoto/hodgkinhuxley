bfde361e9067696c9428d7253951471fb5a5f31e
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2014-11-23 16:37:21 +0900

    add alpha/beta functions

--- simple/hh.c
+++ simple/hh.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef float FLOAT;
+typedef double FLOAT;
+// typedef double FLOAT;
+#define EXP(x) exp(x)
+// #define EXP(x) expf(x)
 
+FLOAT alpha_m (FLOAT v) { return( (25. - v)/(10. * EXP( (25. - v) / 10.) - 1.) ); }
+FLOAT beta_m  (FLOAT v) { return( 4. * EXP(- v / 18.));                           }
+FLOAT alpha_h (FLOAT v) { return( 0.07 * EXP(- v / 20.) );                        }
+FLOAT beta_h  (FLOAT v) { return( 1. / (EXP(30. - v) / 10. + 1.) );               }
+FLOAT alpha_n (FLOAT v) { return( (10. - v)/(100. * EXP((10. - v) / 10.) - 1.) ); }
+FLOAT beta_n  (FLOAT v) { return( 0.125 * EXP(- v / 80.));                        }
 
 int hh(FLOAT stoptime)
 {
@@ -17,7 +26,7 @@ int hh(FLOAT stoptime)
   FLOAT hh_e_na = 115.0;     // [mV]
   FLOAT hh_e_k = -12.0;      // [mV]
 
-  FLOAT hh_v_reset = 10.613; // [mV]
+  FLOAT hh_v_rest = 10.613;  // [mV]
   FLOAT hh_v = 0.0;          // [mV]
 
   FLOAT hh_m = 0.05;
@@ -29,6 +38,7 @@ int hh(FLOAT stoptime)
   FLOAT i_k   = 0.0;
   FLOAT i_m   = 0.0;
   FLOAT i_inj = 0.0;
+
 
 }
 
